26edf6c00dac6e46f6bd9bcfd28e367d4363324f
ivanna18 <88410238+ivanna18@users.noreply.github.com>
2026-05-28 13:20:19 -0600

    Add files via upload

---
+++ backup_biblioteca.sql
@@ -0,0 +1,212 @@
+-- MySQL dump 10.13  Distrib 8.0.46, for Linux (x86_64)
+--
+-- Host: localhost    Database: biblioteca
+-- ------------------------------------------------------
+-- Server version	8.0.46
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `auditoria`
+--
+
+DROP TABLE IF EXISTS `auditoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auditoria` (
+  `id_auditoria` int NOT NULL AUTO_INCREMENT,
+  `accion` varchar(100) NOT NULL,
+  `descripcion` text,
+  `fecha` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_auditoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auditoria`
+--
+
+LOCK TABLES `auditoria` WRITE;
+/*!40000 ALTER TABLE `auditoria` DISABLE KEYS */;
+INSERT INTO `auditoria` VALUES (1,'Nuevo prestamo','Se registro un prestamo para el usuario ID 3 con el prestamo ID 14','2026-05-26 22:57:18'),(2,'Nuevo prestamo','Se registro un prestamo para el usuario ID 2 con el prestamo ID 15','2026-05-26 23:03:42'),(3,'Nuevo prestamo','Se registro un prestamo para el usuario ID 5 con el prestamo ID 16','2026-05-26 23:11:03');
+/*!40000 ALTER TABLE `auditoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `detalle_prestamo`
+--
+
+DROP TABLE IF EXISTS `detalle_prestamo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `detalle_prestamo` (
+  `id_detalle` int NOT NULL AUTO_INCREMENT,
+  `id_prestamo` int NOT NULL,
+  `id_libro` int NOT NULL,
+  `cantidad` int NOT NULL,
+  PRIMARY KEY (`id_detalle`),
+  KEY `fk_prestamo` (`id_prestamo`),
+  KEY `fk_libro` (`id_libro`),
+  CONSTRAINT `fk_libro` FOREIGN KEY (`id_libro`) REFERENCES `libros` (`id_libro`),
+  CONSTRAINT `fk_prestamo` FOREIGN KEY (`id_prestamo`) REFERENCES `prestamos` (`id_prestamo`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detalle_prestamo`
+--
+
+LOCK TABLES `detalle_prestamo` WRITE;
+/*!40000 ALTER TABLE `detalle_prestamo` DISABLE KEYS */;
+INSERT INTO `detalle_prestamo` VALUES (1,1,1,1),(2,1,2,1),(3,2,3,1),(4,3,4,1),(5,4,2,1),(6,5,6,1),(7,5,2,1),(8,6,8,2),(9,7,3,1),(10,8,5,2),(11,8,3,1),(12,8,4,1),(13,9,1,2),(14,10,7,1),(15,11,4,1),(16,12,9,1),(17,15,1,1);
+/*!40000 ALTER TABLE `detalle_prestamo` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `trg_actualizar_stock` AFTER INSERT ON `detalle_prestamo` FOR EACH ROW BEGIN
+    UPDATE libros
+    SET stock = stock - NEW.cantidad
+    WHERE id_libro = NEW.id_libro;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `libros`
+--
+
+DROP TABLE IF EXISTS `libros`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `libros` (
+  `id_libro` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(150) NOT NULL,
+  `autor` varchar(100) NOT NULL,
+  `categoria` varchar(50) DEFAULT NULL,
+  `stock` int NOT NULL,
+  PRIMARY KEY (`id_libro`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `libros`
+--
+
+LOCK TABLES `libros` WRITE;
+/*!40000 ALTER TABLE `libros` DISABLE KEYS */;
+INSERT INTO `libros` VALUES (1,'Cien años de soledad','Gabriel Garcia Marquez','Novela',4),(2,'Clean Code','Robert C. Martin','Programacion',3),(3,'El Principito','Antoine de Saint-Exupery','Infantil',4),(4,'Database Systems','Elmasri','Bases de Datos',2),(5,'Don Quijote de la mancha','Miguel de Cervantes','Novela',6),(6,'The Pragmatic Programmer','Andrew Hunt','Programacion',4),(7,'Harry Potter','J.K. Rowling','Fantasia',6),(8,'Introduction to Algorithms','Cormen','Algoritmos',2),(9,'Orgullo y Prejuicio','Jane Austen','Romance',1),(10,'Artificial Intelligence','Stuart Russell','IA',3),(11,'Redes de Computadoras','Andrew Tanenbaum','Redes',0);
+/*!40000 ALTER TABLE `libros` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `prestamos`
+--
+
+DROP TABLE IF EXISTS `prestamos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `prestamos` (
+  `id_prestamo` int NOT NULL AUTO_INCREMENT,
+  `id_usuario` int NOT NULL,
+  `fecha_prestamo` date NOT NULL,
+  `fecha_limite` date NOT NULL,
+  `estado` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_prestamo`),
+  KEY `fk_usuario` (`id_usuario`),
+  CONSTRAINT `fk_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prestamos`
+--
+
+LOCK TABLES `prestamos` WRITE;
+/*!40000 ALTER TABLE `prestamos` DISABLE KEYS */;
+INSERT INTO `prestamos` VALUES (1,1,'2026-01-20','2026-02-25','Devuelto'),(2,2,'2025-11-10','2026-01-03','Devuelto'),(3,3,'2026-04-18','2026-06-01','Activo'),(4,7,'2025-06-25','2025-08-01','Devuelto'),(5,5,'2026-01-05','2026-01-12','Vencido'),(6,2,'2026-05-20','2026-07-01','Activo'),(7,9,'2026-04-25','2026-06-10','Activo'),(8,4,'2026-03-08','2026-05-02','Devuelto'),(9,6,'2026-02-18','2026-04-10','Vencido'),(10,2,'2026-05-25','2026-06-01','Activo'),(11,8,'2025-09-18','2025-11-02','Devuelto'),(12,1,'2025-10-28','2025-12-06','Devuelto'),(13,10,'2026-03-01','2026-03-10','Vencido'),(14,3,'2026-05-26','2026-06-02','Activo'),(15,2,'2026-05-26','2026-06-05','Activo'),(16,5,'2026-05-26','2026-06-05','Activo');
+/*!40000 ALTER TABLE `prestamos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `trg_auditoria_prestamo` AFTER INSERT ON `prestamos` FOR EACH ROW BEGIN
+    INSERT INTO auditoria (accion, descripcion)
+    VALUES (
+        'Nuevo prestamo',
+        CONCAT(
+            'Se registro un prestamo para el usuario ID ',
+            NEW.id_usuario,
+            ' con el prestamo ID ',
+            NEW.id_prestamo
+        )
+    );
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `tipo_usuario` varchar(30) NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `correo` (`correo`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Ana Lopez','anaaa@gmail.com','8112345678','Alumno'),(2,'Carlos Ruiz','carlos@gmail.com','8187654321','Docente'),(3,'Maria Torres','maria@gmail.com','8123456789','Alumno'),(4,'Luis Hernandez','luis@gmail.com','8111569899','Alumno'),(5,'Sofia Martinez','sofia@gmail.com','8298641137','Docente'),(6,'Diego Perez','diego@gmail.com','838964753','Alumno'),(7,'Ivanna Rodriguez','ivanna18@gmail.com','8186868360','Alumno'),(8,'Antonio Valdez','antonio@gmail.com','8181234980','Alumno'),(9,'Victor Colin','victor@gmail.com','8345611249','Alumno'),(10,'Jorge Islas','jorge@gmail.com','8181349086','Docente');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-05-27  1:02:54
